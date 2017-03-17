981400318bba1e7df38e799a38d450e2612f44e3
unknown <rajeevhathi@gmail.com>
2017-03-17 15:42:55 +0800

    Removed unwanted stuff

--- src/main/java/demo/microprofile/tomeeweb/Expense.java
+++ src/main/java/demo/microprofile/tomeeweb/Expense.java
@@ -1,11 +1,5 @@
 package demo.microprofile.tomeeweb;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-
-//@XmlRootElement
-//@XmlAccessorType(XmlAccessType.FIELD)
 public class Expense {
 	private String category;
 	private Double amount;
